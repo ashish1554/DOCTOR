b9bc426d54a24e32c12c5a228b99d6da7394c9bf
Ashish Pateliya <pateliyaashish7@gmail.com>
2025-02-27 21:10:49 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json())
 
 // app.use(cors())
 app.use(cors({ 
-  origin: "https://doctor-frontend-sz9n.onrender.com/",
+  origin: "https://doctor-frontend-sz9n.onrender.com",
   credentials: true 
 }));
 
